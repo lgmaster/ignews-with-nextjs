d4e37c873ec3acf372d206f5e34a82e16e48303c
Luiz Gustavo Amorim <lgdeveloper@outlook.com>
2021-09-02 16:28:16 -0300

    added SubscribeButton component

---
+++ src/components/SubscribeButton/index.tsx
@@ -0,0 +1,9 @@
+import styles from './styles.module.scss'
+
+export function SubscribeButton() {
+  return (
+    <button type="button" class={styles.subscribeButton}>
+      Subscribe now
+    </button>
+  )
+}
